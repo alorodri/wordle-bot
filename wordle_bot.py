d3601f5314513a06c52372f3d117edb2962a4c98
alorodri <aloro2392@gmail.com>
2022-03-30 23:34:51 +0200

    algoritmo absurdo

--- wordle_bot.py
+++ wordle_bot.py
@@ -2,6 +2,7 @@ from constants import Constants
 import json
 from random import randrange
 import re
+from words import PERCENTS
 
 from test import Test
 
@@ -11,9 +12,10 @@ words: dict = json.load(f)
 class Solver():
 
     __letters_not_in_word = []
-    __letters_in_word = []
-    __word = ''
+    __letters_in_other_pos = {}
+    __letters_correct_pos = [None] * 5
     __test = Test()
+    __tried_letters = {}
 
     def __init__(self) -> None:
         pass
@@ -40,23 +42,77 @@ class Solver():
                     result += match
 
         return result
-                
+
+    @staticmethod
+    def check_is_in_map_list(m, k, v):
+        if k not in m.keys():
+            return False
+        elif v not in m[k]:
+            return False
+        return True
+
+    @staticmethod
+    def add_to_list(m, k, v):
+        if k in m.keys():
+            m[k].append(v)
+        else:
+            m[k] = [v]                
 
     def solve(self, regex):
+        print(f'Starting with pattern {regex}')
         regex_words = self.return_words_regex(regex)
-        rand = randrange(len(regex_words))
-        colors = self.__test.try_attempt(regex_words[rand])
+        if len(regex_words):
+            rand = randrange(len(regex_words))
+            word_attempt = regex_words[rand]
+            if word_attempt:
+                print(f'Trying {word_attempt} (pattern was {regex})')
+                self.__colors = self.__test.try_attempt(word_attempt)
 
-        # Return an object with word like now, and with letters that are in the word but in other position
-        # Save all the letters that aren't in the word, so we don't use them again
+                for idx, color in enumerate(self.__colors):
+                    if color == Constants.YELLOW:
+                        self.add_to_list(self.__letters_in_other_pos, word_attempt[idx], idx)
+                    elif color == Constants.GREEN:
+                        self.__letters_correct_pos[idx] = word_attempt[idx]
+                    elif color == Constants.GRAY:
+                        self.__letters_not_in_word.append(word_attempt[idx])
 
-        if not colors:
+        if not self.__colors:
             return
-        if Constants.GRAY not in colors and Constants.YELLOW not in colors:
+        if Constants.GRAY not in self.__colors and Constants.YELLOW not in self.__colors:
             print('SOLVED')
             return
+
+        word = ''
+        for idx in range(0, 5):
+
+            next_loop = False
+
+            if self.__letters_correct_pos[idx]:
+                word += self.__letters_correct_pos[idx]
+                continue
+
+            for letter, pos in self.__letters_in_other_pos.items():
+                if idx != pos and letter not in word:
+                    word += letter
+                    next_loop = True
+                    break
+
+            if next_loop:
+                continue
+
+            if idx > 0:
+                word += '.'
+                continue
+
+            for letter in PERCENTS.keys():
+                if (letter not in self.__letters_not_in_word
+                    and not self.check_is_in_map_list(self.__letters_in_other_pos, letter, idx)
+                    and not self.check_is_in_map_list(self.__tried_letters, letter, idx)):
+                    word += letter
+                    self.add_to_list(self.__tried_letters, letter, idx)
+                    break
         
-        self.solve('')
+        self.solve(word)
         
 
 solver = Solver()
